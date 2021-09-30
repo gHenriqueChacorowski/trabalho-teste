2df7dbb79d9ac6a41475297f653db369dc2edd48
Henrique Silva Chacorowski <henrique.chacorowski@grupointegrado.br>
2021-09-29 22:45:20 -0300

    correção put

--- test/classTest/methodsPut/putPosts.test.js
+++ test/classTest/methodsPut/putPosts.test.js
@@ -4,15 +4,18 @@ let chai = require("chai"),
 let servicePutGeral = require("../../classService/methodsPut/servicePutGeral");
 
 let data = require("../../../mocks/cadastros/cadastroPost.json");
-data.title = "new title";
 
 describe("CRIAÇÃO DE CENÁRIOS COM UPDATES DE POSTS RETORNANDO SUCESSO E ERRO.", () => {
   context('Realizando testes nos endPoints de put.', () => {
     it('Deverá atualizar o title do post que possui id = 1 então retornar status 200', done => {
+      data.title = "title updated";
+      data.body = "body updated";
       servicePutGeral.updatePost('/posts/1', data, 200, done);
     });
 
     it('Deverá tentar atualizar o title de um post inexistente então retornar status 500', done => {
+      data.title = "title updated";
+      data.body = "body updated";
       servicePutGeral.updatePost('/posts/101', data, 500, done);
     });
   });
